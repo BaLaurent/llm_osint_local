0c5b2dc706e5d5f51f0bb8eed4326b8d69d1673f
BLaurent <d3adlol0@gmail.com>
2024-08-20 16:57:39 +0200

    Update llm.py
    changed to use local llm

--- llm_osint/llm.py
+++ llm_osint/llm.py
@@ -3,8 +3,9 @@ from langchain.chat_models.base import BaseChatModel
 
 LLMModel = BaseChatModel
 
-default_fast_llm_options = dict(model_name="gpt-4o-mini", request_timeout=120, max_retries=10, temperature=0.7)
-default_llm_options = dict(model_name="gpt-4o", request_timeout=120, max_retries=10, temperature=0.7)
+# I use a fixed name for the models to use so you can change it by simply recreating a model from a file using ollama create
+default_fast_llm_options = dict(base_url="http://localhost:11434/v1", api_key="bidon" ,model_name="llm_osint_fast", request_timeout=120, max_retries=10, temperature=0.7)
+default_llm_options = dict(base_url="http://localhost:11434/v1", api_key="bidon", model_name="llm_osint", request_timeout=120, max_retries=10, temperature=0.7)
 
 
 def get_default_fast_llm() -> LLMModel:
